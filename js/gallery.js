4411474560c4ef89415dbf857a65a2d457f569bb
xdd44-utopia <xdd44.utopia@gmail.com>
2021-11-30 07:14:15 +0800

    add scroll function

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,60 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var currentPage = 0;
+var num = 0;
+var lastScroll = 0;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+
+document.onkeydown = onKeyDown;
+window.onload = setUp();
+
+function setUp() {
+    num = document.getElementsByClassName('container').length;
+    setInterval(checkScroll, 1000);
+    currentPage = 0;
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    slides[0].style.marginLeft = `${-100 * slideIndex + 100}vw`;
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+function onKeyDown(event) {
+    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(event.code) > -1) {
+        event.preventDefault();
     }
-    //slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    var event = event || window.event;
+    switch (event.keyCode) {
+        case 38:
+            if (currentPage > 0) {
+                currentPage--;
+                scroll(currentPage);
+            }
+            break;
+        case 87:
+            if (currentPage > 0) {
+                currentPage--;
+                scroll(currentPage);
+            }
+            break;
+        case 40:
+            if (currentPage < num - 1) {
+                currentPage++;
+                scroll(currentPage);
+            }
+            break;
+        case 83:
+            if (currentPage < num - 1) {
+                currentPage++;
+                scroll(currentPage);
+            }
+            break;
+    }
 }
 
+function scroll(index) {
+    console.log(index);
+    document.getElementsByClassName('container')[index].scrollIntoView();
+}
+
+function checkScroll() {
+    for (let i = 0; i < num; i++) {
+        if (document.getElementsByClassName('container')[i].getBoundingClientRect().y >= -10) {
+            currentPage = i;
+            break;
+        }
+    }
+}
